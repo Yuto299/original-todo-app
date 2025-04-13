d9898e139d95e03e608c01767ccf8e871f2c1652
Yuto299 <ota.se.ke@outlook.jp>
2025-04-13 22:24:43 +0900

    削除機能実装

--- main.js
+++ main.js
@@ -4,7 +4,32 @@ const list = document.getElementById('incomplete-list');
 
 addButton.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.textContent = input.value;
-  list.appendChild(li); //ulの子要素としてliを追加
+
+  // テキスト部分の表示
+  const inText = document.createElement('span');
+  inText.textContent = input.value;
+
+  // 削除ボタンの作成
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = '削除';
+
+  // liにspanと削除ボタンを追加
+  li.appendChild(inText);
+  li.appendChild(deleteButton);
+
+  // ulにliを追加
+  list.appendChild(li);
+
+  // 入力欄を空にする
   input.value = '';
+
+  // 削除ボタンを押したときの処理
+  deleteButton.addEventListener('click', () => {
+    const answer = confirm('本当に削除しますか？');
+    if (answer) {
+      list.removeChild(li);
+    } else {
+      alert('キャンセルされました。');
+    }
+  });
 });
